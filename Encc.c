44d3f0a3696cacc9e87116eb64ec6648e9a5309d
ta1yaki13 <r0mesds1205@yahoo.co.jp>
2020-07-23 23:12:03 +0900

    再帰下降構文解析と、x86-64のレジスタマシンにスタックマシンのエミュレートの実装

--- Encc.c
+++ Encc.c
@@ -1,35 +1,281 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+// Tokenizer /////////////////////////////////////////////////////
+
+// トークンの種類
+typedef enum {
+    TK_RESERVED,  // 記号
+    TK_NUM,       // 整数トークン
+    TK_EOF,       // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+    TokenKind kind; // トークンの型
+    Token *next;    // 次の入力のトークン
+    int val;        // kindがTK_NUMの場合、その数値
+    char *str;      // トークン文字列
+};
+
+// 現在着目しているトークン
+Token *token;
+
+// 入力プログラム
+char *user_input;
+
+// エラーを報告するための関数
+// printfと同じ引数をとる
+void error(char *fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// エラーを報告するための関数
+void error_at(char *loc, char *fmt, ...){
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, "");    // pos個の空白を出力
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// 次のトークンが期待している記号のときには、トークンを１つ読み進めて
+// 真を返す。それ以外の場合は偽を返す。
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        return false;
+    token = token->next;
+    return true;
+}
+
+// 次のトークンが期待している記号の時には、トークンを１つ読み進める。
+// それ以外の場合にはエラーを報告する。
+void expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        error_at(token->str, "'%c'ではありません", op);
+    token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを１つ読み進めてその数値を返す。
+// それ以外の場合には、エラーを報告する。
+int expect_number() {
+    if (token->kind != TK_NUM)
+        error_at(token->str, "数ではありません");
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成してcurに繋げる.
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+// 入力文字列pをトークナイズして返す
+Token *tokenize() {
+    char *p = user_input;
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        // 空白文字をスキップ
+        if(isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if(ispunct(*p)) {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+
+        error_at(p, "トークナイズできません。");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
+
+
+// Parser ///////////////////////////////////////////////////////
+
+
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD,     // +
+    ND_SUB,     // -
+    ND_MUL,     // *
+    ND_DIV,     // /
+    ND_NUM,     // 整数
+}   NodeKind;
+
+
+// 抽象構文木のノード型
+typedef struct Node Node;
+struct Node {
+    NodeKind kind;  // ノードの型
+    Node *lhs;      // 左辺
+    Node *rhs;      // 右辺
+    long val;        // kindがND_NUMの場合のみ使う
+};
+
+static Node *new_node(NodeKind kind);
+static Node *binary(NodeKind kind, Node *lhs, Node *rhs);
+static Node *new_num(int val);
+
+// 左辺と右辺を受け取る2項演算子
+static Node *new_node(NodeKind kind) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+// 左辺と右辺を受け取る2項演算子
+static Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = new_node(kind);
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+static Node *new_num(int val) {
+    Node *node = new_node(ND_NUM);
+    node->val = val;
+    return node;
+}
+
+static Node *expr(void);
+static Node *mul(void);
+static Node *primary(void);
+
+// パーサ（左結合の演算子をパーズする）
+static Node *expr(void){
+    Node *node = mul();
+
+    for(;;){
+        if (consume('+'))
+            node = new_binary(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_binary(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+// expr関数で使用するmul関数
+static Node *mul(void) {
+    Node *node = primary();
+
+    for(;;){
+        if (consume('*'))
+            node = new_binary(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_binary(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+// mul関数でしようするprimary関数
+static Node *primary(void) {
+    // 次のトークンが"("なら、"(" expr ")"のはず
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    // そうでなければ数値のはず
+    return new_num(expect_number());
+}
+
+// スタックマシンをレジスタマシンにエミュレートする関数
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("  sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("  imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("  cqo\n");
+        printf("  idiv rdi\n");
+        break;
+    }
+
+    printf("  push rax\n");
+}
+
+
+// main関数 /////////////////////////////////////
+
+// main関数です。
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "引数の個数が正しくありません\n");
+        error("引数の個数が正しくありません");
         return 1;
     }
 
-    char *p = argv[1];
+   user_input = argv[1];
+   token = tokenize();
+   Node *node = expr();
 
+    // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-    while (*p) {
-        if (*p == '+') {
-            p++;
-            printf("  add rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        }
-        
-        if (*p == '-') {
-            p++;
-            printf("  sub rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        }
-        fprintf(stderr, "予期しない文字です。: '%c'\n", *p);
-        return 1;
-    }
-    
+    // 抽象構文木を下りながらコードを生成
+    gen(node);
+
+    // スタックトップに式全体の値が残っていると思うので、
+    // スタックトップ内の値をRAXにロードして関数からの返り値とする。
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
